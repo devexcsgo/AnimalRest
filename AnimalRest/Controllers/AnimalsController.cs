2208023885dc4e9f6fb745de18fcbde0a4139449
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-10-08 23:24:30 +0200

    lavet crud på dyr

--- AnimalRest/Controllers/AnimalsController.cs
+++ AnimalRest/Controllers/AnimalsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnimalLibrary;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,46 @@ namespace AnimalRest.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
+        private AnimalRepositoryList _animalRepositoryList;
+
+        public AnimalsController(AnimalRepositoryList animalRepositoryList)
+        {
+            _animalRepositoryList = animalRepositoryList;
+        }
+
         // GET: api/<AnimalsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_animalRepositoryList.GetAll());
         }
 
         // GET api/<AnimalsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Animal Get(int id)
         {
-            return "value";
+            return _animalRepositoryList.GetById(id);
         }
 
         // POST api/<AnimalsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Animal Post([FromBody] Animal value)
         {
+            return _animalRepositoryList.Add(value);
         }
 
         // PUT api/<AnimalsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public Animal Put(int id, [FromBody] Animal value)
         {
+            return _animalRepositoryList.Update(id, value);
         }
 
         // DELETE api/<AnimalsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public Animal Delete(int id)
         {
+            return _animalRepositoryList.Remove(id);
         }
     }
 }
